cd59d201b2055a2dd4fc8a28792ef29bb2aec13c
maevaPasteur <maeva.pasteur.pro@gmail.com>
2025-09-01 18:47:12 +0200

    refacto(about): timeline experiences rename

--- pages/about.vue
+++ pages/about.vue
@@ -2,11 +2,11 @@
     <AboutIntroduction/>
     <AboutSkillsTimeline/>
     <AboutCompetences/>
-    <AboutTimeline/>
+    <AboutExperiencesTimeline/>
 </template>
 
 <script setup lang="ts">
-import AboutTimeline from "@/components/organisms/AboutTimeline.vue";
+import AboutExperiencesTimeline from "@/components/organisms/AboutExperiencesTimeline.vue";
 import AboutIntroduction from "@/components/organisms/AboutIntroduction.vue";
 import AboutCompetences from "@/components/organisms/AboutCompetences.vue";
 import AboutSkillsTimeline from "@/components/organisms/AboutSkillsTimeline.vue";
