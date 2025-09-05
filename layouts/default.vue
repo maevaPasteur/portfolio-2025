944d3448fd4f9489fa4f78d2980d1bc7c684479a
maevaPasteur <maeva.pasteur.pro@gmail.com>
2025-09-06 00:04:27 +0200

    feat(seo): add href lang

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,4 +9,28 @@
 
 <script setup lang="ts">
 import Header from "@/components/sections/Header.vue";
+
+const { $i18n } = useNuxtApp()
+const route = useRoute()
+
+const hreflangs = $i18n.locales.map((locale: any) => {
+  const localePath = useLocalePath()
+  const href = `https://maevapasteur.com${localePath(route.path, locale.code)}`
+  
+  return {
+    rel: 'alternate',
+    hreflang: locale.code,
+    href: href
+  }
+})
+
+hreflangs.push({
+  rel: 'alternate', 
+  hreflang: 'x-default',
+  href: `https://maevapasteur.com${useLocalePath()(route.path, $i18n.defaultLocale)}`
+})
+
+useHead({
+  link: hreflangs
+})
 </script>
